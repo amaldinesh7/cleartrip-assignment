46dfd4bbc65ef57124542e6e18a358020143db29
Amal Dinesh <amalkdinesh@gmail.com>
2023-10-04 12:06:17 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+const App = () => {
+  return (
+    <div className="">
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+};
+
+export default App;
